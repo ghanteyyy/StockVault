61c049a27209c18526186fc68091d7d440bfa2ef
ghanteyyy <ghanteyyy@gmail.com>
2025-06-07 14:51:16 +0545

    Added url endpoint for login and dashboard page

--- Users/urls.py
+++ Users/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage, name='index'),
+    path('logout/', views.Logout, name='logout'),
     path('login/', views.LoginPage, name='login'),
     path('register/', views.SignupPage, name='signup'),
+    path('dashboard/', views.Dashboard, name='dashboard'),
 ]
