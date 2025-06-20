e04eaf50c7a5a9b8c25da5d9a0a487f2f2a461c3
ghanteyyy <ghanteyyy@gmail.com>
2025-06-20 23:26:11 +0545

    Removed fetch_models.py

--- Users/fetch_models.py
+++
@@ -1,8 +0,0 @@
-from . import models
-from .serializers import *
-
-
-def fetch_recent_activities(request):
-    activities = models.RecentActivities.objects.filter(user_id=request.user)
-
-    return RecentActivitiesSerializer(activities, many=True).data
