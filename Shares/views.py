42d3814121bbfbf8b4ee2607cb8a44b845cf9a89
ghanteyyy <ghanteyyy@gmail.com>
2025-06-21 02:39:09 +0545

    Added functionality to add activities trigger by the user

--- Shares/views.py
+++ Shares/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import models as share_models
+from Users import models as user_models
 
-# Create your views here.
+
+def AddToRecentActivities(request, company_id, activity_message):
+    new_activity = share_models.RecentActivities(user_id=request.user, company_id=company_id, activity=activity_message)
+
+    new_activity.save()
