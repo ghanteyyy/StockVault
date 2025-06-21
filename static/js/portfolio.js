d13aa148734b932b83f6e01a7ac8c6f73e898780
ghanteyyy <ghanteyyy@gmail.com>
2025-06-21 10:57:34 +0545

    Added UI for portfolio page

---
+++ static/js/portfolio.js
@@ -0,0 +1,8 @@
+// Restrict user to input non-numeric values
+share_numberings = document.querySelectorAll('.share_numbering');
+
+share_numberings.forEach(share_numbering => {
+    share_numbering.addEventListener("input", (event)=>{
+        event.target.value = event.target.value.replace(/\D/g, "");
+    })
+});
