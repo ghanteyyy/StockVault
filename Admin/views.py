4266928809a841f61d97cdf736bedbad8ae8a9ed
ghanteyyy <ghanteyyy@gmail.com>
2025-07-18 20:48:43 +0545

    Render AdminPage only if any admin user is logged in

--- Admin/views.py
+++ Admin/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def AdminPage(request):
     return render(request, 'admin/dashboard.html', {'page_title': 'Admin | Stock Vault'})
