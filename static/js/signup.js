4fdefcdf74914d3eefe833d93ff3ec6f73e34acc
ghanteyyy <ghanteyyy@gmail.com>
2025-08-03 18:36:14 +0545

    Updated validation for dob and gender

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,8 +1,18 @@
+window.onload = function() {
+    const today = new Date();
+    const maxDate = new Date(today.getFullYear() - 13, today.getMonth(), today.getDate());
+    const formattedMaxDate = maxDate.toISOString().split('T')[0];
+
+    document.getElementById('dob').max = formattedMaxDate;
+};
+
+
 function validateForm(event) {
     const fullName = document.getElementById('fullName').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const dob = document.getElementById('dob').value;
+    const gender = document.getElementById('gender').value;
     const profilePicture = document.getElementById('profilePicture').files;
     const nameError = document.getElementById('name-error');
     const emailError = document.getElementById('email-error');
@@ -42,15 +52,17 @@ function validateForm(event) {
     }
 
     // Validate date of birth
-    const dobRegex = /^\d{4}\/\d{2}\/\d{2}$/;
+    const dobRegex = /^\d{4}[-\/](0[1-9]|1[0-2])[-\/](0[1-9]|[12][0-9]|3[01])$/;
+
     if (!dob || !dobRegex.test(dob)) {
         dobError.style.display = 'block';
         hasErrors = true;
     } else {
-        const [year, month, day] = dob.split('/').map(Number);
+        const [year, month, day] = dob.split('-').map(Number);
         const today = new Date();
-        const birthDate = new Date(year, month - 1, day);
+        const birthDate = new Date(year, month, day);
         const age = today.getFullYear() - birthDate.getFullYear();
+
         if (age > 110 || birthDate > today || isNaN(birthDate.getTime())) {
             dobError.style.display = 'block';
             hasErrors = true;
