6d71a4cf898e572973cf31a56d2c9060cad38ce7
ghanteyyy <ghanteyyy@gmail.com>
2025-08-08 22:43:30 +0545

    Added url endpoint for target page

--- Users/urls.py
+++ Users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.HomePage, name='index'),
     path('logout/', views.Logout, name='logout'),
     path('login/', views.LoginPage, name='login'),
+    path('target/', views.TargetPage, name='target'),
     path('buy-sell/', views.BuySell, name='buy-sell'),
     path('register/', views.SignupPage, name='signup'),
     path('dashboard/', views.Dashboard, name='dashboard'),
