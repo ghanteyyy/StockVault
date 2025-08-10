f60ababb10168d8003e6640d341fed5aa2898bfb
ghanteyyy <ghanteyyy@gmail.com>
2025-08-10 19:52:29 +0545

    Added logics and stylings for editing target

--- static/js/target.js
+++ static/js/target.js
@@ -27,3 +27,68 @@ function validateTargetForm(){
 
     return true;
 }
+
+
+function validateEditTargetForm() {
+    const low_target = document.getElementById('edit_low_target').value.trim();
+    const high_target = document.getElementById('edit_high_target').value.trim();
+    const errorMessageEl = document.querySelector('.edit-error-message');
+
+    // Regex for numbers
+    const number_regex = /^\d*\.?\d+$/;
+
+    // Reset error
+    errorMessageEl.style.display = 'none';
+    errorMessageEl.textContent = '';
+
+    if (!low_target || !high_target) {
+        showError('Please complete all required fields before submitting');
+        return false;
+    }
+
+    if (!number_regex.test(low_target) || !number_regex.test(high_target)) {
+        showError('Please enter valid numbers for the target prices.');
+        return false;
+    }
+
+    if (parseFloat(low_target) >= parseFloat(high_target)) {
+        showError('Low target must be less than high target.');
+        return false;
+    }
+
+    return true;
+
+    function showError(message) {
+        errorMessageEl.textContent = message;
+        errorMessageEl.style.display = 'block';
+    }
+}
+
+
+function displayEditTargetForm(target) {
+    const editContainer = document.querySelector('.edit_target');
+    editContainer.style.display = 'flex';
+
+    // Stop closing modal when clicking inside the form
+    editContainer.querySelector('.edit').addEventListener('click', e => e.stopPropagation());
+
+    // Click outside closes modal
+    editContainer.addEventListener('click', hideEditTargetForm);
+
+    // Read data attributes
+    const data_company_name = target.getAttribute('data-company_name');
+    const data_abbreviation = target.getAttribute('data-abbreviation');
+    const data_low_target = target.getAttribute('data-low_target');
+    const data_high_target = target.getAttribute('data-high_target');
+
+    // Fill modal fields
+    document.querySelector('.company_title').textContent = `${data_company_name} (${data_abbreviation})`;
+    document.getElementById('edit_company_abbr').value = data_abbreviation;
+    document.getElementById('edit_low_target').value = data_low_target;
+    document.getElementById('edit_high_target').value = data_high_target;
+}
+
+function hideEditTargetForm() {
+    document.querySelector('.edit_target').style.display = 'none';
+}
+
