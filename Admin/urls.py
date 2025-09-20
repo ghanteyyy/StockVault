a4668ee28956e027012981e3309e1ee2e496a432
ghanteyyy <ghanteyyy@gmail.com>
2025-09-20 22:32:45 +0545

    Added endponint to delete user

--- Admin/urls.py
+++ Admin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', views.AdminPage, name='admin-index'),
     path('admin/user/add', views.AddUser, name='admin-user-add'),
     path('admin/user/edit', views.EditUser, name='admin-user-edit'),
+    path('admin/user/delete', views.DeleteUser, name='admin-user-delete'),
 ]
