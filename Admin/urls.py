fa34a0f18170bc98b5c050190c20fb416fdd876e
ghanteyyy <ghanteyyy@gmail.com>
2025-09-20 15:57:42 +0545

    Added new endpoints for adding and editing user information

--- Admin/urls.py
+++ Admin/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('admin/', views.AdminPage, name='admin-index')
+    path('admin/', views.AdminPage, name='admin-index'),
+    path('admin/user/add', views.AddUser, name='admin-user-add'),
+    path('admin/user/edit', views.EditUser, name='admin-user-edit'),
 ]
