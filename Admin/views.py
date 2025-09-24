0ab1ff05880cf5667d63daff0217a02176c8783f
ghanteyyy <ghanteyyy@gmail.com>
2025-09-24 20:52:25 +0545

    Added views for listed company modifications

--- Admin/views.py
+++ Admin/views.py
@@ -1,6 +1,7 @@
 import datetime
 from django.shortcuts import render
 import Users.models as user_models
+import Shares.models as share_models
 from django.http import JsonResponse
 from django.db.models import CharField
 from django.db.models.functions import Cast, TruncDate
@@ -93,3 +94,71 @@ def DeleteUser(request):
 
     except user_models.CustomUser.DoesNotExist:
         return JsonResponse({'status': False, 'message': 'User not found'})
+
+
+@login_required(login_url='login')
+def AdminListedCompanies(request):
+    companies = share_models.ListedCompanies.objects.all().values_list("name", "sector", "id", "abbreviation")
+
+    table_heads = ['Symbol', 'Sector', 'Actions']
+
+    context = {
+        'companies': companies,
+        'table_heads': table_heads,
+        'page_title': 'Admin | Stock Vault',
+    }
+
+    return render(request, 'admin/companies.html', context)
+
+
+@login_required(login_url='login')
+def AdminListedCompaniesAdd(request):
+    if request.method.lower() == 'post':
+        company_name = request.POST.get('company_name', '').strip()
+        company_sector = request.POST.get('company_sector', '').strip()
+        company_abbreviation = request.POST.get('company_abbreviation', '').strip()
+
+        if share_models.ListedCompanies.objects.filter(name=company_name):
+            return JsonResponse({'status': False, 'message': 'Company already exists'})
+
+        company = share_models.ListedCompanies(name=company_name, sector=company_sector, abbreviation=company_abbreviation)
+        company.save()
+
+        return JsonResponse({'status': True, 'message': 'Cpmpany added successfully'})
+
+    return JsonResponse({'status': False, 'message': 'Something went wrong'})
+
+
+@login_required(login_url='login')
+def AdminListedCompaniesEdit(request):
+    if request.method.lower() == 'post':
+        company_id = request.POST.get('company_id', '').strip()
+        company_name = request.POST.get('company_name', '').strip()
+        company_sector = request.POST.get('company_sector', '').strip()
+        company_abbreviation = request.POST.get('company_abbreviation', '').strip()
+
+        if not share_models.ListedCompanies.objects.filter(id=company_id):
+            return JsonResponse({'status': False, 'message': 'Company does not exist'})
+
+        company = share_models.ListedCompanies.objects.get(id=company_id)
+        company.name = company_name
+        company.sector = company_sector
+        company.abbreviation = company_abbreviation
+        company.save()
+
+        return JsonResponse({'status': True, 'message': 'Company information updated successfully'})
+
+    return JsonResponse({'status': False, 'message': 'Something went wrong'})
+
+
+@login_required(login_url='login')
+def AdminListedCompaniesDelete(request):
+    company_id = request.GET.get('company_id', '').strip()
+
+    if not share_models.ListedCompanies.objects.filter(id=company_id):
+        return JsonResponse({'status': False, 'message': 'Company does not exist'})
+
+    company = share_models.ListedCompanies.objects.get(id=company_id)
+    company.delete()
+
+    return JsonResponse({'status': True, 'message': 'Company deleted successfully'})
