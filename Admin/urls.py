2ccf787fe6ce6d13ef87ba9b9531a57e24b909b9
ghanteyyy <ghanteyyy@gmail.com>
2025-09-24 20:50:48 +0545

    Added endpoints for handling companies modifications

--- Admin/urls.py
+++ Admin/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('admin/user/add', views.AddUser, name='admin-user-add'),
     path('admin/user/edit', views.EditUser, name='admin-user-edit'),
     path('admin/user/delete', views.DeleteUser, name='admin-user-delete'),
+
+    path('admin/companies', views.AdminListedCompanies, name='admin-companies'),
+    path('admin/company/add', views.AdminListedCompaniesAdd, name='admin-company-add'),
+    path('admin/company/edit', views.AdminListedCompaniesEdit, name='admin-company-edit'),
+    path('admin/company/delete', views.AdminListedCompaniesDelete, name='admin-company-delete'),
 ]
