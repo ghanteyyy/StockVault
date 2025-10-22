f4ee5ee42e63267bee4bed2a3f8e5c3fecd67cb8
ghanteyyy <ghanteyyy@gmail.com>
2025-10-22 13:11:31 +0545

    Fixed toggling between open and close hamburger buttons

--- static/js/navs/sidebar.js
+++ static/js/navs/sidebar.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let hamburger_values = ['☰', '✖'];
+
     const hamburgerBtn = document.querySelector('#hamburger-btn');
     const sidebar = document.querySelector('.sidebar');
     const mainContent = document.querySelector('.main-content');
 
     hamburgerBtn.addEventListener('click', () => {
-        sidebar.classList.toggle('open');
+        hamburger_values = hamburger_values.reverse();
+
         hamburgerBtn.classList.toggle('open');
+        hamburgerBtn.innerHTML = hamburger_values[0];
+
+        sidebar.classList.toggle('open');
         mainContent.classList.toggle('shifted');
     });
 
