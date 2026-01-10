b1404f9d371087bc0c5a34c7b9c96e9d4a71ce0d
ghanteyyy <ghanteyyy@gmail.com>
2026-01-11 00:22:08 +0545

    Added url endpoints for shares models

---
+++ Shares/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('api/nepse/indices/', views.NepseIndices),
+    path('api/portfolios/', views.Portfolio.as_view()),
+    path('api/wishlists/', views.Wishlist.as_view()),
+    path('api/companies/', views.ListedCompanies),
+]
