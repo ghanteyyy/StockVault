ee8a5ad505e7b7f94db906a3db8234099b38a99e
ghanteyyy <ghanteyyy@gmail.com>
2026-01-11 00:28:36 +0545

    Added JWT tokens endpoints Included url endpoints for shares and accounts apps

--- core/urls.py
+++ core/urls.py
@@ -18,13 +18,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('Users.urls')),
     path('', include('Admin.urls')),
-    path('api/', include('api.urls')),
+    path('', include('Shares.urls')),
+    path('', include('accounts.urls')),
     path("captcha/", include("captcha.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
